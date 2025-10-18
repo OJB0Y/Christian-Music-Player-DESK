a7329e01e74c00a5d9f2f73cadae05013e38dc4c
OJB0Y <oraneignaciojuan27@gmail.com>
2025-10-18 11:38:43 -0400

    Fix filename case for 'Te Necesito' song

--- script.js
+++ script.js
@@ -500,7 +500,7 @@ const playlist = [
   {
     title: "Te Necesito",
     artist: "Los Hermanos Reyes de Guatemala",
-    src: "songs/Te Necesito.mp3",
+    src: "songs/Te necesito.mp3",
     cover: "images/Hay una ciudad.png"
   }
   //add more songs under here {}
